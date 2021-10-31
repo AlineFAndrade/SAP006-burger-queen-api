c323ab226f0beaed9826ca7b438cb4003da2593a
Aline Andrade <marson.aline@gmail.com>
2021-10-31 11:34:40 -0300

    swagger como pagina inicial

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,13 +1,17 @@
-const { Router } = require('express')
-const UsersRouter = require("./usersRouter")
-const OrdersRouter = require("./ordersRouter")
-const ProductsRouter = require("./productsRouter")
+const { Router } = require("express");
+const UsersRouter = require("./usersRouter");
+const OrdersRouter = require("./ordersRouter");
+const ProductsRouter = require("./productsRouter");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("../../swagger.json");
 
-const router = Router()
+const router = Router();
 
 // aqui vai todas as rotas
-router.use('/users', UsersRouter);
-router.use('/orders', OrdersRouter);
-router.use('/products', ProductsRouter);
+router.use("/users", UsersRouter);
+router.use("/orders", OrdersRouter);
+router.use("/products", ProductsRouter);
+router.use("/", swaggerUi.serve);
+router.get("/", swaggerUi.setup(swaggerDocument));
 
-module.exports = router
+module.exports = router;
